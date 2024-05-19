323fe84a037b9783c3be33411e92b4787db411c8
AlexIancu71 <iancualex71@gmail.com>
2024-05-19 19:16:53 +0300

    added flori.py

---
+++ flori.py
@@ -0,0 +1,39 @@
+rom flask import Flask, url_for
+
+#from app.lib import network
+#from app.lib import ubuntu
+#from app.grafice.exemplu_func_grad_2 import valori_x, valori_y, genereaza_grafice
+
+print('flori')
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=['GET'])
+def index():
+    ret = "<h1>flori</h1>"
+
+
+    ret += "<pre>"
+
+    ret += " - test1\n"
+    ret += " - test2"
+    
+    ret += "</pre>"
+    
+    ret += " - test3\n"
+    ret += " - test4"
+    
+    return "flori"
+    
+@app.cli.command()
+def test():
+    """
+    Rulare 'unit tests'
+    
+    Apelare pytest din aplicatia systest, cu ajutorul comenzii flask.
+    
+    """
+    import pytest
+    sys.exit(pytest.main(["."]))
