52efceeb6e5d81ebf4856e70ca3dfd0449d4b103
AlexIancu71 <iancualex71@gmail.com>
2024-05-19 18:47:48 +0300

    .

--- app/lib/biblioteca_flori.py
+++ app/lib/biblioteca_flori.py
@@ -1,1 +1,8 @@
 #In curs de dezvoltare
+def culoare_Dalia():
+	raspuns = "Daliile se gasesc in aproape toate culorile, cu excepția albastrului!"
+	return raspuns
+    
+def descriere_Dalia():
+        raspuns = "Dalia este o plantă puternică, abundentă și cu flori lungi, care reprezintă un plus de culoare în grădina de vară sau în balconul dumneavoastră."
+	return raspuns
