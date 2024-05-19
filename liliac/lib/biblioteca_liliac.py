1ec701c07bda5d1899b2e00ec7b2f7a88203288a
snouragan <74413033+snouragan@users.noreply.github.com>
2024-05-19 08:54:27 -0700

    change name from app to liliac

---
+++ liliac/lib/biblioteca_liliac.py
@@ -0,0 +1,11 @@
+def descriere_liliac():
+    return "Liliacul (Syringa vulgaris) face parte din familia oleaceelor și este o specie din genul Syringa care înflorește primăvara. Este un arbust a cărui înălțime poate ajunge până la șapte metri, cu ramuri drepte și lujeri puțin muchiați."
+
+def frunze_liliac():
+    return "Frunzele sunt ovate sau lat-ovate, la bază cordate, până la 12 centimetri lungime, acuminate, pețiol circa 2,5 cm lungime, glabre, verzi-întunecat."
+
+def floare_liliac():
+    return "Florile sunt simple sau duble, plăcut mirositoare, grupate în penicule de până la 20 cm lungime, multiflorale, în culori diferite, de la liliachiu la alb. Floarea are caliciul mic, campanulat, cu patru dinți, corolă cu tub de un centimetru lungime, cu patru lobi patenți și două stamine."
+
+def culoare_liliac():
+    return "Lila este o culoare asemănătoare cu violetul, dar o nuanță mai deschisă, spre roz. Numele provine de la florile de liliac, care au această culoare."
---
+++ liliac/liliac.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+from lib.biblioteca_liliac import descriere_liliac, frunze_liliac, floare_liliac, culoare_liliac
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    descriere = descriere_liliac()
+
+    return render_template('index.html', descriere=descriere)
+
+@app.route('/frunze')
+def frunze():
+    frunze = frunze_liliac()
+
+    return render_template('frunze.html', frunze = frunze)
+
+@app.route('/floare')
+def floare():
+    floare = floare_liliac()
+
+    return render_template('floare.html', floare = floare)
+
+@app.route('/culoare')
+def culoare():
+    culoare = culoare_liliac()
+
+    return render_template('culoare.html', culoare = culoare)
+
+if __name__ == '__main__':
+
+    app.run()
