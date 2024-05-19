35abe10400a01dcc4038f3160ddddceecdd62a1f
Xpert1700 <stefancripers@gmail.com>
2024-05-19 16:26:30 +0300

    ghiocel.py

---
+++ libs.py
@@ -0,0 +1,5 @@
+def descriere_ghiocel():
+    return "Ghiocelul (Galanthus L.; din limba greaca: gala - lapte, anthos - floare) este un gen de aproximativ 20 de specii de plante erbacee perene bulboase din familia Amaryllidaceae, plante care înfloresc printre primele la începutul primăverii. Plantele au două frunze liniare și o singură floare mică, albă, care atârnă în formă de clopot, cu șase petale în două cercuri ( verticil). Petalele interioare mai mici au semne verzi. "
+def culoare_ghiocel():
+    return "Culoarea e de obicei alba"
+    
