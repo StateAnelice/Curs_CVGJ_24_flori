71611d67845b297855d5e6f6007c5d1bb0e10148
conacheantonio <conache.antonio1@yahoo.com>
2024-05-19 17:33:01 +0300

    Conache Antonio Mihaita commited files for Margareta

--- app/lib/biblioteca_flori.py
+++
@@ -1,1 +0,0 @@
-#In curs de dezvoltare
--- app/lib/libs.py
+++
@@ -1,5 +0,0 @@
-def descriere_brandusa():
-    return "Brândușa de munte este o plantă perenă, cu flori violete sau albe, care crește în regiunile montane."
-
-def culoare_brandusa():
-    return "Florile brândușei de munte sunt de obicei violete, dar pot fi și albe."
