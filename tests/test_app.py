767b867b9e913813f1810fb1b195cd41d8937108
Xpert1700 <stefancripers@gmail.com>
2024-05-19 18:01:58 +0300

    ghiocel.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -9,18 +9,6 @@ class BasicTests(unittest.TestCase):
         # propagate the exceptions to the test client
         self.ghiocel.testing = True
 
-    def test_index(self):
-        # sends HTTP GET request to the ghiocellication
-        result = self.ghiocel.get('/')
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Flori', result.data)
-
-    def test_ghiocel(self):
-        result = self.ghiocel.get('/ghiocel')
-        self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Ghiocel', result.data)
-
     def test_ghiocel_descriere(self):
         result = self.ghiocel.get('/ghiocel/descriere')
         self.assertEqual(result.status_code, 200)
@@ -30,6 +18,8 @@ class BasicTests(unittest.TestCase):
         result = self.ghiocel.get('/ghiocel/culoare')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Culoare', result.data)
+
+
 
 if __name__ == "__main__":
     unittest.main()
