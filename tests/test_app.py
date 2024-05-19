d50f1547d963cd9385fe92c8d5d0cb31f4226492
Xpert1700 <stefancripers@gmail.com>
2024-05-19 18:06:22 +0300

    ghiocel.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -3,21 +3,14 @@ from ghiocel import ghiocel
 
 class BasicTests(unittest.TestCase):
 
-    def setUp(self):
-        # create a test client
-        self.ghiocel = ghiocel.test_client()
-        # propagate the exceptions to the test client
-        self.ghiocel.testing = True
 
-    def test_ghiocel_descriere(self):
-        result = self.ghiocel.get('/ghiocel/descriere')
-        self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Descriere', result.data)
 
-    def test_ghiocel_culoare(self):
-        result = self.ghiocel.get('/ghiocel/culoare')
+    def test_index(self):
+        result = self.ghiocel.get('/')
         self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Culoare', result.data)
+        self.assertIn(b'index', result.data)
+
+
 
 
 
