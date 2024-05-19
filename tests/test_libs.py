0113a91828fac5aa6070115832d1520411bb48a3
AlexIancu71 <iancualex71@gmail.com>
2024-05-19 19:11:35 +0300

    added test

--- tests/test_libs.py
+++ tests/test_libs.py
@@ -1,15 +1,30 @@
-import unittest
-from lib.libs import descriere_brandusa, culoare_brandusa
+import logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
-class TestBiblioteca(unittest.TestCase):
+import sys
+sys.path.append("../")
 
-    def test_descriere_brandusa(self):
-        descriere = descriere_brandusa()
-        self.assertEqual(descriere, "Brândușa de munte este o plantă perenă, cu flori violete sau albe, care crește în regiunile montane.")
+import lib.biblioteca_fructe as fructe
 
-    def test_culoare_brandusa(self):
-        culoare = culoare_brandusa()
-        self.assertEqual(culoare, "Florile brândușei de munte sunt de obicei violete, dar pot fi și albe.")
 
-if __name__ == "__main__":
-    unittest.main()
+
+def test_culoare_Dalia():
+    culoare = fructe.culoare_Dalia()
+
+    if "toate culorile" in culoare:
+        logger.info(f"Functia culoare_Dalia functioneaza corect: {culoare}")
+        assert True
+    else:
+        logger.error(f"Functia culoare_Dalia NU functioneaza corect: {culoare}")
+        assert False
+
+def test_descriere_Dalia():
+    descriere = fructe.descriere_acai()
+
+    if "plus de culoare" in descriere:
+        logger.info(f"Functia descriere_Dalia functioneaza corect:\n{descriere}")
+        assert True
+    else:
+        logger.error(f"Functia descriere_Dalia NU functioneaza corect:\n{descriere}")
+        assert False 
