c67f01de5aeaa705b65abd7faa77afdf41573e3f
Dobre Sebastian <dobre.sebi17@gmail.com>
2024-05-19 16:38:02 +0300

    commit din idea

--- app/lib/libs.py
+++ app/lib/libs.py
@@ -1,5 +1,5 @@
 def descriere_brandusa():
-    return "Brândușa de munte este o plantă perenă, cu flori violete sau albe, care crește în regiunile montane."
+    return "Brândușa de munte este o plantă scundă din familia Iridaceae. Are o singură tulpină mică, înconjurată la bază de mai multe teci cu două-trei frunze alungite și înguste. La vârful tulpinii se găsește o singură floare violet-deschisă cu o dungă mai întunecată la vârful florii.."
 
 def culoare_brandusa():
     return "Florile brândușei de munte sunt de obicei violete, dar pot fi și albe."
---
+++ app/tests/test_app.py
@@ -0,0 +1,33 @@
+import unittest
+from app.brandusa import app
+
+class BasicTests(unittest.TestCase):
+    # Executat înainte de fiecare test
+    def setUp(self):
+        app.config['TESTING'] = True
+        app.config['DEBUG'] = False
+        self.app = app.test_client()
+
+    # Verifică că pagina principală se încarcă corect
+    def test_main_page(self):
+        response = self.app.get('/', follow_redirects=True)
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Brandusa', response.data)
+
+    def test_color_page(self):
+        response = self.app.get('/color', follow_redirects=True)
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Brandusa', response.data)
+
+    def test_description_page(self):
+        response = self.app.get('/description', follow_redirects=True)
+        print(response.data)
+        self.assertEqual(response.status_code, 200)
+        self.assertIn(b'Brandusa', response.data)
+
+
+# Permite rularea testelor cu 'python test_app.py'
+if __name__ == "_main_":
+    unittest.main()
