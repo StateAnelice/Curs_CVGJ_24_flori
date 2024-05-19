d55374c22eced5c61fd9dea8f14f1909eac25041
Voicu Florin <florin_ioan.voicu@stud.etti.upb.ro>
2024-05-19 22:10:13 +0300

    hortensia final

--- app/lib/libs.py
+++ app/lib/libs.py
@@ -1,5 +1,5 @@
 def descriere_hortensie():
-    return "Hortensia, (/haɪˈdreɪndʒiə/)[3] numită în mod obișnuit hortensia, este un gen de peste 75 de specii de plante cu flori originare din Asia și America. De departe, cea mai mare diversitate de specii se află în estul Asiei, în special în China, Coreea și Japonia."
+    return "Hortensia, numită în mod obișnuit hortensia, este un gen de peste 75 de specii de plante cu flori originare din Asia și America. De departe, cea mai mare diversitate de specii se află în estul Asiei, în special în China, Coreea și Japonia."
 
 def culoare_hortensie():
     return "Florile hortensiei sunt de obicei albe, albastre sau roz."
