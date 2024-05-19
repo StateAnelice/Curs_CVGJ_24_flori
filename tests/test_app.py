16848f6525e08ff0a4c46fe6c9f00d7892fe424a
snouragan <74413033+snouragan@users.noreply.github.com>
2024-05-19 08:03:31 -0700

    test update

--- tests/test_app.py
+++ tests/test_app.py
@@ -4,37 +4,27 @@ from app.liliac import liliac
 class BasicTests(unittest.TestCase):
 
     def setUp(self):
-        # create a test client
         self.liliac = liliac.test_client()
-        # propagate the exceptions to the test client
         self.liliac.testing = True
 
-    def test_index(self):
-        # sends HTTP GET request to the liliaclication
+    def test_liliac_descriere(self):
         result = self.liliac.get('/')
-        # assert the status code of the response
-        self.assertEqual(result.status_code, 200)
-        self.assertIn(b'Flori', result.data)
-
-
-    def test_brandusa_descriere(self):
-        result = self.brandusa.get('/descriere')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Descriere', result.data)
 
-    def test_brandusa_frunze(self):
-        result = self.brandusa.get('/frunze')
+    def test_liliac_frunze(self):
+        result = self.liliac.get('/frunze')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Frunze', result.data)
 
-    def test_brandusa_floare(self):
-        result = self.brandusa.get('/floare')
+    def test_liliac_floare(self):
+        result = self.liliac.get('/floare')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Floare', result.data)
 
 
-    def test_brandusa_culoare(self):
-        result = self.brandusa.get('/culoare')
+    def test_liliac_culoare(self):
+        result = self.liliac.get('/culoare')
         self.assertEqual(result.status_code, 200)
         self.assertIn(b'Culoare', result.data)
 
