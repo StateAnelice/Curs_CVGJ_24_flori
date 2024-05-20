5776900e1c3beeb6b3ee9bdb61611aa036a0cd27
mihailcostin <166308886+mihailcostin@users.noreply.github.com>
2024-05-20 07:27:36 +0300

    Update test.py

--- app/test.py
+++ app/test.py
@@ -2,28 +2,35 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
+
+
 import sys
 sys.path.append("../")
+
+
 
 import lib.biblioteca_flori as flori
 
 
-def test_culoare_lavanda():
+
+def test_culoare_orhidee():
 	culoare = flori.culoare_lavanda()
 	
 	if"Florile pot avea culoarea violet închis sau albă." in culoare:
-		logger.info(f"Functia culoare_lavanda functioneaza corect: {culoare}")
+		logger.info(f"Functia culoare_orhidee functioneaza corect: {culoare}")
 		assert True
 	else:
-		logger.error(f"Functia culoare_lavanda NU functioneaza corect: {culoare}")
+		logger.error(f"Functia culoare_orhidee NU functioneaza corect: {culoare}")
 		assert False
 
-def test_descriere_lavanda():
+
+
+def test_descriere_orhidee():
 	descriere = flori.descriere_lavanda()
 	
 	if "Lavanda este un gen de plante din familia Lamiaceae" in descriere:
-		logger.info(f"Functia descriere_lavanda functioneaza corect:\n{descriere}")
+		logger.info(f"Functia descriere_orhidee functioneaza corect:\n{descriere}")
 		assert True
 	else:
-		logger.error(f"Functia descriere_lavanda NU functioneaza corect:\n{descriere}")
+		logger.error(f"Functia descriere_orhidee NU functioneaza corect:\n{descriere}")
 		assert False
