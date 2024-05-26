f9af66c629670c0996548e9f76b8652dc404c246
MariusDutu <mariusdutu.x@gmail.com>
2024-05-26 14:33:53 +0300

    cod

---
+++ app/lib/biblioteca_flori.py
@@ -0,0 +1,8 @@
+
+def culoare_papadie():
+	ret = "Culoarea unei flori de papadie este galbena."
+	return ret
+    
+def descriere_papadie():
+	ret = "Păpădia (Taraxacum officinale) este o plantă erbacee din familia compozitelor, cu frunze lungi, crestate și cu flori galbene grupate în capitule. Mai este numită și buhă, bășina porcului, cicoare, crestățea, lăptucă, lilicea, mâță, papalungă, pilug, turci, curu-găinii, floarea-broaștei, floarea-găinii, floarea-mălaiului, floarea-sorului, floarea-turcului, flori-galbene, gălbinele-grase, gușa-găinii, ouăle-găinilor, papa-găinii, părăsita-găinilor sau pui-de-gâscă."
+	return ret
