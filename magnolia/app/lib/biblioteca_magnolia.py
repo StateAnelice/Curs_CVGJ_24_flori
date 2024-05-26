87be7c6b17c0298fcfb561e24d3d95621b3706dc
conacheantonio <conache.antonio1@yahoo.com>
2024-05-26 22:02:42 +0300

    Third commit

--- magnolia/app/lib/biblioteca_magnolia.py
+++
@@ -1,8 +0,0 @@
-def descriere_magnolia():
-    return "Magnolia is a large genus of about 210 to 340 flowering plant species in the subfamily Magnolioideae of the family Magnoliaceae. The natural range of Magnolia species is disjunct, with a main center in east and southeast Asia and a secondary center in eastern North America, Central America, the West Indies, and some species in South America."
-
-def culoare_magnolia():
-    return "Magnolias are spreading evergreen or deciduous trees or shrubs characterised by large fragrant flowers, which may be bowl-shaped or star-shaped, in shades of white, pink, purple, green, or yellow."
-
-def anotimp_magnolia():
-    return "In deciduous species, the blooms often appear before the leaves in spring. Cone-like fruits are often produced in the autumn."
